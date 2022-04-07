f4ceb4b037eefe2f5987dd4a40bcbe7bcee5d0a0
juda-star <juda.ishta@gmail.com>
2022-04-07 14:58:37 +0300

    deploy3

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use(express.static(__dirname + "./dist/e-commerce-angular"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/e-commerce-angular/index.html"));
-});
-app.listen(process.env.PORT || 8080);
-
 // const express = require("express");
 // const path = require("path");
-
 // const app = express();
+// app.use(express.static(__dirname + "./dist/e-commerce-angular"));
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/dist/e-commerce-angular/index.html"));
+// });
+// app.listen(process.env.PORT || 8080);
 
-// // Serve only the static files form the dist directory
-// app.use(express.static("./dist/e-commerce-angular"));
+const express = require("express");
+const path = require("path");
 
-// app.get("/*", (req, res) =>
-//   res.sendFile("index.html", { root: "dist/e-commerce-angular" })
-// );
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/e-commerce-angular"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/e-commerce-angular" })
+);
